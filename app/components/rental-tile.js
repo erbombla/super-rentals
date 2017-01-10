d411d8d1189273a2d07fddfa3c64f993a4d9a018
Eric Bomblatus <ebomblatus@gmail.com>
2017-01-09 18:02:34 -0800

    add delete option

--- app/components/rental-tile.js
+++ app/components/rental-tile.js
@@ -5,9 +5,14 @@ export default Ember.Component.extend({
   actions: {
     imageShow: function() {
       this.set('isImageShowing', true);
-    }, 
+    },
     imageHide: function() {
       this.set('isImageShowing', false);
+    },
+    delete(rental) {
+      if (confirm('Are you sure you want to delete this rental?')) {
+        this.sendAction('destroyRental', rental);
+      }
     }
   }
 });
